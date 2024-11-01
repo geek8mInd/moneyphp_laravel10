554eb6ee8f0f785b6d3898f2c8f84335fc31224b
geek8mInd <rheabonifacio@yahoo.com>
2024-11-01 22:30:22 +0800

    Add files via upload
    adjust template for conversion and implement money object representation on currencies module

--- resources/views/conversion/compute.blade.php
+++ resources/views/conversion/compute.blade.php
@@ -29,10 +29,10 @@
             @csrf
             <div class="mb-3">
                 <label class="form-label" for="currency">Base Currency:</label>
-                <select name="currency" id="currency" class="form-control @error('currency') is-invalid @enderror" placeholder="Currency">
+                <select name="currency" id="currency" class="form-control @error('currency') is-invalid @enderror" placeholder="Currency" @disabled(true)>
                     <option>-- Please select currency --</option>
                     @foreach($currencies as $currency)
-                        <option value="{{ $currency->currency_iso_code }}" {{ old('currency') == $currency->currency_iso_code ? "selected" : "" }}>
+                        <option value="{{ $currency->currency_iso_code }}" {{ $oldcurrency == $currency->currency_iso_code ? "selected" : "" }}>
                             {{ $currency->currency_name }}
                         </option>
                         @endforeach
@@ -44,10 +44,10 @@
 
             <div class="mb-3">
                 <label class="form-label" for="next_currency">Convert To:</label>
-                <select name="next_currency" id="next_currency" class="form-control @error('next_currency') is-invalid @enderror" placeholder="Currency">
+                <select name="next_currency" id="next_currency" class="form-control @error('next_currency') is-invalid @enderror" placeholder="Currency" @disabled(true)>
                     <option>-- Please select currency --</option>
                     @foreach($currencies as $currency)
-                        <option value="{{ $currency->currency_iso_code }}" {{ old('next_currency') == $currency->currency_iso_code ? "selected" : "" }}>
+                        <option value="{{ $currency->currency_iso_code }}" {{ $next_currency == $currency->currency_iso_code ? "selected" : "" }}>
                             {{ $currency->currency_name }}
                         </option>
                         @endforeach
@@ -65,7 +65,8 @@
                     id="amount"
                     class="form-control @error('amount') is-invalid @enderror" 
                     placeholder="Amount"
-                    value={{ old('amount')}}>
+                    value={{ $amount }}
+                    @readonly(true)>
                 @error('amount')
                     <span class="text-danger">{{ $message }}</span>
                 @endif
@@ -79,14 +80,26 @@
                     id="conversion_ratio"
                     class="form-control @error('conversion_ratio') is-invalid @enderror" 
                     placeholder="Conversion Ratio"
-                    value={{ old('conversion_ratio')}}>
+                    value={{ $conversion_ratio }}
+                    @readonly(true)>
                 @error('conversion_ratio')
                     <span class="text-danger">{{ $message }}</span>
                 @endif
             </div>
      
+            <div class="mb-3 bg-info">
+                <label class="form-label" for="result">Result:</label>
+                <input 
+                    type="text" 
+                    name="result" 
+                    id="result"
+                    class="form-control" 
+                    placeholder="Result"
+                    value={{ $result }} @readonly(true)>
+            </div>
+
             <div class="mb-3">
-                <button class="btn btn-success btn-submit">Submit</button>
+                <a class="btn btn-primary" href="{{ route('conversion.index') }}"> Back</a>
             </div>
         </form>
     </div>
